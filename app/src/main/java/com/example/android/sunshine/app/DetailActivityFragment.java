74971d2f160a4b393b0108380abe79e454bf3b1c
santus444 <santosh@Santoshs-MacBook-Pro.local>
2015-06-15 16:34:53 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivityFragment.java
@@ -0,0 +1,30 @@
+package com.example.android.sunshine.app;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DetailActivityFragment extends Fragment {
+
+    public DetailActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        Intent forecastIntent = getActivity().getIntent();
+        String forecastString = forecastIntent.getStringExtra(Intent.EXTRA_TEXT);
+        TextView textView = (TextView) rootView.findViewById(R.id.weatherForecast);
+        textView.setText(forecastString);
+        return rootView;
+    }
+}
